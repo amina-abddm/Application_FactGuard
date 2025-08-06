763a90d24814703979220de20f06653c7665ea0e
bafodej <bafode.jaiteh4@gmail.com>
2025-08-06 12:39:09 +0200

    add install

--- factguard_project/dashboard/views.py
+++ factguard_project/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def dashboard_view(request):
+    return HttpResponse("<h1>Dashboard FactGuard</h1><p>Application en cours de développement</p>")
+    return HttpResponse("Hello, world. You're at the dashboard index.") 
