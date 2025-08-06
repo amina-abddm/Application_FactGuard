83e01614198cae1aea5adafa9ed0e016764fab51
RyadMoha <ryadmohamedi@outlook.fr>
2025-08-06 16:25:45 +0200

    Page accueil

--- factguard_project/factguard_project/settings.py
+++ factguard_project/factguard_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',  
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
