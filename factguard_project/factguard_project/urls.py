4a5bae0308281f377dc42c95d5c781c87f7ac6d1
bafodej <bafode.jaiteh4@gmail.com>
2025-08-07 12:21:50 +0200

    gestion on conflict urls.py

--- factguard_project/factguard_project/urls.py
+++ factguard_project/factguard_project/urls.py
@@ -19,11 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-<<<<<<< HEAD
     path("home/", include("home.urls")),
-    path('admin/', admin.site.urls),
-=======
     path("admin/", admin.site.urls),
     path("dashboard/", include("dashboard.urls")),
->>>>>>> feature/dashboard
 ]
